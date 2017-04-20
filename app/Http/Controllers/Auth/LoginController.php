1035f07be0afdbe64b4a97a90ff224fe7a307d49
Daniel Campodonico <dcampodonico@folderit.com>
2017-04-20 10:00:31 -0300

    Agregada librería jwt-auth y controllers para login y register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,36 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+    protected $jwtauth;
 
     /**
      * Create a new controller instance.
-     *
-     * @return void
+     * @param JWTAuth $jwtauth
      */
-    public function __construct()
+    public function __construct(JWTAuth $jwtauth)
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->jwtauth = $jwtauth;
+        $this->middleware('jwt.auth', ['except' => ['login']]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->json()->all();
+
+        try {
+            if (! $token = $this->jwtauth->attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
     }
 }
